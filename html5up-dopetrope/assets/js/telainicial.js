9f3105cf7d08fb4e1f9a081e74c672c5e481fa36
Natalia <nataliabento97@gmail.com>
2025-02-12 16:10:20 -0300

    alteções nos textos e layouts iniciais

--- html5up-dopetrope/assets/js/telainicial.js
+++ html5up-dopetrope/assets/js/telainicial.js
@@ -5,36 +5,37 @@ document.addEventListener("DOMContentLoaded", function () {
   let identifierInput = document.getElementById("identifier");
   let googleLogin = document.getElementById("googleLogin");
   let toggleContainer = document.getElementById("toggleContainer");
+  let loginForm = document.getElementById("loginForm");
+  let perfil = "viajante";
 
   toggleContainer.addEventListener("click", function () {
-    if (toggleBtn.innerText === "Viajante") {
-      // Mudar para Empreendedor
+    if (perfil === "viajante") {
+      perfil = "empreendedor";
       toggleBtn.style.transform = "translateX(100%)";
-      toggleBtn.innerText = "Empreendedor";
-      toggleBtn.style.backgroundColor = "#2E3192";
+      toggleBtn.style.backgroundColor = "#E7467F";
       loginTitle.innerText = "Login - Empreendedor";
 
-      // Trocar Email por CNPJ
       identifierLabel.innerText = "CNPJ";
       identifierInput.setAttribute("placeholder", "Digite seu CNPJ");
       identifierInput.setAttribute("type", "text");
 
-      // Esconder botão do Google
       googleLogin.style.display = "none";
     } else {
-      // Mudar para Viajante
+      perfil = "viajante";
       toggleBtn.style.transform = "translateX(0)";
-      toggleBtn.innerText = "Viajante";
       toggleBtn.style.backgroundColor = "#E7467F";
       loginTitle.innerText = "Login - Viajante";
 
-      // Trocar CNPJ por Email
       identifierLabel.innerText = "E-mail";
       identifierInput.setAttribute("placeholder", "Digite seu e-mail");
       identifierInput.setAttribute("type", "email");
 
-      // Mostrar botão do Google
       googleLogin.style.display = "block";
     }
+  });
+
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    window.location.href = "index.html?perfil=" + perfil;
   });
 });
