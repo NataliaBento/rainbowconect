ffe5666d2d51c6eb2f5979b8d1736bb1855fb7b0
NataliaBento <nataliabento97@gmail.com>
2025-02-17 15:35:47 -0300

    feat: mudança de animação

--- html5up-dopetrope/assets/js/animacao.js
+++ html5up-dopetrope/assets/js/animacao.js
@@ -1,8 +1,38 @@
-document.addEventListener("scroll", function () {
-  let scrollPos = window.scrollY;
-  let animacaoElemento = document.getElementById("animacao-viagem");
+document.addEventListener("DOMContentLoaded", function () {
+    const icon = document.getElementById("dvd-icon");
+    let speedX = 3; 
+    let speedY = 3; 
+    let posX = Math.random() * window.innerWidth; 
+    let posY = Math.random() * window.innerHeight; 
 
-  if (animacaoElemento) {
-    animacaoElemento.style.transform = `translateX(${scrollPos * 0.3}px)`;
-  }
+    function moveIcon() {
+        posX += speedX;
+        posY += speedY;
+
+        if (posX + icon.clientWidth >= window.innerWidth || posX <= 0) {
+            speedX *= -1; 
+            changeColor(); 
+        }
+
+        if (posY + icon.clientHeight >= window.innerHeight || posY <= 0) {
+            speedY *= -1; 
+            changeColor(); 
+        }
+
+        icon.style.left = posX + "px";
+        icon.style.top = posY + "px";
+
+        requestAnimationFrame(moveIcon);
+    }
+
+    function changeColor() {
+        const colors = ["#FF4500", "#FFD700", "#2F4F4F", "#E53359", "#302F90", "#29ABE2"];
+        icon.style.color = colors[Math.floor(Math.random() * colors.length)];
+    }
+
+    icon.addEventListener("click", function () {
+        icon.style.display = "none"; 
+    });
+
+    moveIcon();
 });
